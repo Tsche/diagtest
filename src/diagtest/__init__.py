affc4079b34a296f41f43ae91a8691b62a1b6eb1
Tsche <Tsche@users.noreply.github.com>
2024-01-02 12:23:35 +0100

    --list-commands option

--- src/diagtest/__init__.py
+++ src/diagtest/__init__.py
@@ -9,11 +9,22 @@ from diagtest.driver import Runner
 setup_logger()
 
 @click.command()
+@click.option("--list-compilers", type=bool, is_flag=True, default=False, help="Print discovered compilers and exit.")
 @click.option("--output", type=Optional[Path], default=None, help="Path to build directory")
 @click.option("--verbose", type=bool, is_flag=True, default=False, help="Be more verbose")
-@click.argument("filename", type=Path)
-def main(filename: Path, verbose = False, output: Optional[Path] = None):
-    assert filename.exists(), "Please provide a valid path"
+@click.argument("files", type=Path, nargs=-1)
+def main(files: list[Path], list_compilers=False, verbose=False, output: Optional[Path]=None):
+    #assert filename.exists(), "Please provide a valid path"
     if verbose:
         logging.root.setLevel(logging.DEBUG)
-    Runner(filename, output).run()
+
+    if list_compilers:
+        from diagtest.default import dump_compilers
+        dump_compilers()
+        return
+
+    if not files:
+        raise RuntimeError("Input files missing")
+
+    for path in files:
+        Runner(path, output).run()
--- src/diagtest/default.py
+++ src/diagtest/default.py
@@ -20,3 +20,25 @@ def get_defaults(language: str):
                 **{compiler.__name__.lower(): compiler(language=language)
                    for compiler in compilers}}
     return {}
+
+def dump_compilers():
+    from diagtest.compilers.gcc import GCC
+    from diagtest.compilers.clang import Clang
+    from diagtest.compilers.msvc import MSVC
+
+    compilers = [GCC, Clang, MSVC]
+    for compiler in compilers:
+        discovered = compiler.discover()
+        if not discovered:
+            continue
+
+        for compiler_info in discovered:
+            version = compiler.get_version(compiler_info.executable)
+            print(f"{compiler.__name__} ({version['version']})")
+            print(f"  Executable: {compiler_info.executable}")
+            print(f"  Target:     {version['target']}")
+            print( "  Languages:")
+            standards = compiler.get_supported_standards(compiler_info.executable)
+            for language, standard in standards.items():
+                print(f"    {language:<6} {', '.join(', '.join(std) for std in standard)}")
+            print()
